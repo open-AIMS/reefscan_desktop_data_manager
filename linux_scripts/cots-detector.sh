3ea97b828a30d412a3437e83e4bf1ccfc1535555
gcoleman72 <g.coleman@aims.gov.au>
2023-10-06 16:28:00 +1000

    don't process COTS again#28

--- linux_scripts/cots-detector.sh
+++ linux_scripts/cots-detector.sh
@@ -2,6 +2,13 @@
 cd ~/ccip-cv-pipeline/cv-pipeline/
 source cv-pipeline-env/bin/activate
 
+{
+if [ -d $2/final ]; then
+    echo "COTS processing already done"
+    exit 0
+fi
+}
+
 python3 inference.py -p ../models/ccip-deploy-mar23-1344x768_ubuntu_x86_64/ccip-deploy-mar23-1344x768_ubuntu_x86_64.json -pp StoreProcessingResultPostProcessing -i "$1" -r "$2"
 
 last_sub_folder=$(find $2 -maxdepth 1 -mindepth 1 -type d | tail -n 1)
