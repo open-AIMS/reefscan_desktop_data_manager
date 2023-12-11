57314b81bce6677fd8046e47c467cb048bf61a78
gcoleman72 <g.coleman@aims.gov.au>
2023-12-11 13:45:44 +1000

    one-dir and lazy loading

--- linux_scripts/cots-detector.sh
+++ linux_scripts/cots-detector.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-cd ~/ccip-cv-pipeline/cv-pipeline/
+cd ~/ccip-cv-pipeline/ccip-cv-pipeline/cv-pipeline
 source cv-pipeline-env/bin/activate
 
-{
-if [ -d $2/final ]; then
-    echo "COTS processing already done"
-    exit 0
-fi
-}
-
-python3 inference.py -p ../models/ccip-deploy-mar23-1344x768_ubuntu_x86_64/ccip-deploy-mar23-1344x768_ubuntu_x86_64.json -pp StoreProcessingResultPostProcessing -i "$1" -r "$2"
+rm -r $2/final
+python3 inference.py -p ../models/ccip-seg-nov23_ubuntu_x86_64/ccip-seg-nov23_ubuntu_x86_64.json -pp StoreProcessingResultPostProcessing -i "$1" -r "$2"
 
 last_sub_folder=$(find $2 -maxdepth 1 -mindepth 1 -type d | tail -n 1)
 
-mv $last_sub_folder $2/final
+cp -r $last_sub_folder $2/final
