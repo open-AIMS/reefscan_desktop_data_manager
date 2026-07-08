7769ee9a00968ca6222a19e397801daff743ec3b
gcoleman72 <g.coleman@aims.gov.au>
2026-07-08 14:16:11 +1000

    rmove ccip from install process. Use Brano's repo instead.

--- linux_scripts/cots-detector.sh
+++ linux_scripts/cots-detector.sh
@@ -18,11 +18,10 @@ if [ "$#" -ne 2 ]; then
 fi
 
 source ~/.reefscan_env
-cd $REEFSCAN_HOME/ccip-cv-pipeline/cv-pipeline
-source cv-pipeline-env/bin/activate
+cd $COTS_HOME
 
 rm -r "$2/final"
-python3 inference.py -p ../models/ccip-seg-nov24_ubuntu2004_x86_64/ccip-seg-nov24_ubuntu2004_x86_64.json -pp StoreProcessingResultPostProcessing -i "$1" -r "$2"
+pixi run run-inference -- "$1" "$2"
 
 last_sub_folder=$(find "$2" -maxdepth 1 -mindepth 1 -type d | tail -n 1)
 
